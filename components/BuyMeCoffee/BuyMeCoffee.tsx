a830229df6e2d1b528b700d7068470f73946547a
KonstantinVVictoria <konstantinvictoria@gmail.com>
2022-11-21 22:30:39 -0800

    added buy me coffee

---
+++ components/BuyMeCoffee/BuyMeCoffee.tsx
@@ -0,0 +1,8 @@
+"use client";
+export function BuyMeCoffee() {
+  return (
+    <a href="https://www.buymeacoffee.com/konstantinv">
+      <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=konstantinv&button_colour=FF5F5F&font_colour=ffffff&font_family=Lato&outline_colour=000000&coffee_colour=FFDD00" />
+    </a>
+  );
+}
